2dd79cd7a725694292968a7c6ef208a87aedd4f7
bcb <benjamincb@hotmail.dk>
2023-09-06 12:13:15 +0200

    prep for lecture2

--- src/Lecture2.hs
+++ src/Lecture2.hs
@@ -4,7 +4,11 @@ import Prelude hiding (product)
 
 main :: IO ()
 main = do
+    putStrLn $ "product [] = " ++ show (product [])
     putStrLn $ "product [1..5] = " ++ show (product [1..5])
+    putStrLn $ "second [1,4,5,6] = " ++ show (second [1,4,5,6])
+    putStrLn $ "second [\"some\", \"bizzare\", \"mango\"] = " ++ show (second ["some", "bizzare", "mango"])
+    putStrLn $ "second [1] = " ++ show (second [1])
 
 -- prep sheet says to use 0 for empty list, but that does not make any sense to me
 product :: (Num a) => [a] -> a
@@ -12,6 +16,6 @@ product [] = 1
 product (x : xs) = x * product xs
 
 second :: [a] -> a
-second [] = undefined
-second [_] = undefined
+-- second [] = undefined
+-- second [_] = undefined
 second (_ : x : _) = x
