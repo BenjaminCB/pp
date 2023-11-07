3b9d0649b422b476dc23aa63ff65808b49e340f7
bcb <benjamincb@hotmail.dk>
2023-11-07 13:13:17 +0100

    complete exercises for lecture 9

---
+++ src/Lecture9.hs
@@ -0,0 +1,47 @@
+module Lecture9 where
+
+main :: IO ()
+main = do
+    letters "Hello"
+    letters' "World"
+
+l :: IO ()
+l = getLine >>= letters
+
+letters :: String -> IO ()
+letters = mapM_ (putStrLn . (:[]))
+
+letters' :: String -> IO ()
+letters' = sequence_ . map (putStrLn . (:[]))
+
+letters'' :: String -> IO ()
+letters'' "" = return ()
+letters'' (c:cs) = do
+    putStrLn [c]
+    letters'' cs
+
+hugorm :: IO ()
+hugorm = do
+    n <- readLn
+    xs <- getNums n
+    print $ sum xs
+
+getNums :: Int -> IO [Int]
+getNums = sequence . flip replicate readLn
+
+sumInts :: Int -> IO Int
+sumInts n = do
+    m <- readLn
+    if m == 0
+        then return n
+        else sumInts (n + m)
+
+whileIO :: IO a -> (a -> Bool) -> (a -> b -> b) -> b -> IO b
+whileIO io p f b = do
+    a <- io
+    if p a
+        then whileIO io p f (f a b)
+        else return b
+
+sumInts' :: Int -> IO Int
+sumInts' = whileIO readLn (/=0) (+)
